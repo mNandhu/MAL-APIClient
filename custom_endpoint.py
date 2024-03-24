339e6209472687301dd5801ef4dc1670bbea7009
Kiruthik Nandhan Murthi <38995539+mNandhu@users.noreply.github.com>
2024-03-24 19:33:27 +0530

    Version 1 - Basic Functionality

---
+++ custom_endpoint.py
@@ -0,0 +1,88 @@
+import json
+import requests
+import secrets
+
+
+class EndPoint:
+    def __init__(self, client_id, authorisation_code=None):
+        self.CLIENT_ID = client_id
+        self.authorisation_code = authorisation_code
+        self.code_verifier = self.get_new_code_verifier()
+
+    # 1. Generate a new Code Verifier / Code Challenge.
+    @staticmethod
+    def get_new_code_verifier() -> str:
+        token = secrets.token_urlsafe(100)
+        return token[:128]
+
+    # 2. Print the URL needed to authorise your application.
+    def print_new_authorisation_url(self):
+        url = (f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={self.CLIENT_ID}'
+               f'&code_challenge={self.code_verifier}')
+        print(f'Authorise your application by clicking here: {url}\n')
+
+    # 3. Once you've authorised your application, you will be redirected to the webpage you've
+    #    specified in the API panel. The URL will contain a parameter named "code" (the Authorisation
+    #    Code). You need to feed that code to the application.
+    def generate_new_token(self) -> dict:
+        url = 'https://myanimelist.net/v1/oauth2/token'
+        data = {
+            'client_id': self.CLIENT_ID,
+            'code': self.authorisation_code,
+            'code_verifier': self.code_verifier,
+            'grant_type': 'authorization_code'
+        }
+
+        response = requests.post(url, data)
+        response.raise_for_status()  # Check whether the request contains errors
+
+        token = response.json()
+        response.close()
+        print('Token generated successfully!')
+
+        with open('token.json', 'w') as file:
+            json.dump(token, file, indent=4)
+            print('Token saved in "token.json"')
+
+        return token
+
+    # 4. Test the API by requesting your profile information
+    @staticmethod
+    def print_user_info(access_token: str):
+        url = 'https://api.myanimelist.net/v2/users/@me'
+        response = requests.get(url, headers={
+            'Authorization': f'Bearer {access_token}'
+        })
+
+        response.raise_for_status()
+        user = response.json()
+        response.close()
+
+        print(f"\n>>> Accessed {user['name']}! <<<")
+
+    @staticmethod
+    def add_entry(access_token: str, anime_id: int, status: str):
+        url = f'https://api.myanimelist.net/v2/anime/{anime_id}/my_list_status'
+        data = {'status': status}
+        response = requests.put(url, data=data, headers={
+            'Authorization': f'Bearer {access_token}'
+        })
+        response.raise_for_status()
+        out = response.json()
+        print(out)
+        response.close()
+
+
+if __name__ == '__main__':
+    with open('.env', 'r') as f:
+        CLIENT_ID = f.readline().strip()
+        auth_code = f.readline().strip()
+
+    test = EndPoint(CLIENT_ID)  # Create an instance of the class
+
+    test.print_new_authorisation_url()  # Print the URL needed to authorise the application
+    # auth_code = input("Enter the authorisation code: ")
+    test.authorisation_code = auth_code  # Set the authorisation code (get from file or input)
+
+    user_token = test.generate_new_token()  # Generate a new token
+    test.print_user_info(user_token['access_token'])  # Test the API by requesting the user's profile information
--- main.py
+++ main.py
@@ -1,55 +1,94 @@
 from mal import client  # Get the MAL list
 import csv  # Read the aniwatch list
 import pickle  # Save the MAL list
+import custom_endpoint
 
-mal_username = 'mNandhu'  # Your MAL username
-get_new_list = 0  # Set to 1 if you want to get a new list from MAL
 
-aniwatch_file_path = 'aniwatch_data.txt'  # Path to the aniwatch export file
+def get_mal_dict(CLIENT_ID, username, get_new_list):
+    # MAL Anime List
+    if get_new_list:
+        cli = client.Client(CLIENT_ID)
+        mal_list = cli.get_anime_list(mal_username, fields=['id'], limit=350, include_nsfw=True)
 
-# MAL Anime List
-if get_new_list:
-    with open('.env', 'r') as f:  # Get the client id from the .env file
-        client_id = f.readline()
-    cli = client.Client(client_id)
-    mal_list = cli.get_anime_list(mal_username, fields=['id'], limit=350, include_nsfw=True)
+        mal_dict = {i['node']['id']: i['node']['title'] for i in mal_list.raw['data']}  # Clean the data
 
-    mal_dict = {i['node']['id']: i['node']['title'] for i in mal_list.raw['data']}  # Clean the data
+        with open('mal_list.data', 'wb') as g:  # Save the data to a file for future use
+            pickle.dump(mal_dict, g)
+    else:
+        with open('mal_list.data', 'rb') as g:  # Load the data from the file if you get_new_list is set to 0
+            mal_dict = pickle.load(g)
+    return mal_dict
 
-    with open('mal_list.data', 'wb') as g:  # Save the data to a file for future use
-        pickle.dump(mal_dict, g)
-else:
-    with open('mal_list.data', 'rb') as g:  # Load the data from the file if you get_new_list is set to 0
-        mal_dict = pickle.load(g)
 
-# Aniwatch animelist
-with open(aniwatch_file_path, 'r') as file:
-    data = list(csv.reader(file, delimiter='|'))
-aniwatch_list = [entry for entry in data if not entry[0].startswith('#')]  # Clean the data
-aniwatch_dict = {int(i[1][i[1].rfind('/') + 1:]): i[0] for i in aniwatch_list}  # dict with id as key and title as value
+def get_aniwatch_dict(file_path):
+    # Aniwatch animelist
+    with open(file_path, 'r') as g:
+        data = list(csv.reader(g, delimiter='|'))
+    aniwatch_list = [entry for entry in data if not entry[0].startswith('#')]  # Clean the data
+    return {int(i[1][i[1].rfind('/') + 1:]): i[0] for i in aniwatch_list}  # dict with id as key and title as value
 
-print(f"Number of entries in MAL: {len(mal_dict)}")
-print(f"Number of entries in Aniwatch: {len(aniwatch_dict)}")
 
-print(f"Missing Entries ({len(aniwatch_dict) - len(mal_dict)}):")
+def find_missing_entries(dict1, dict2):
+    """
+    Find the entries in dict2 that are not in dict1
+    :param dict1: {anime_id: anime_title, ...}
+    :param dict2: {anime_id: anime_title, ...}
+    :return:  {anime_id: anime_title, ...} where anime_id is in dict2 but not in dict1
+    """
+    missing_entries = {}
+    for element in dict2:
+        if element not in dict1:
+            missing_entries[element] = dict2[element]
+    return missing_entries
 
-print('-' * 50)
-print("In AniWatch but not in MAL:")
-for element in aniwatch_dict:
-    if element not in mal_dict:
-        print(mal_dict[element], element)
-print('-' * 50)
 
-print('-' * 50)
-print("In MAL but not in Aniwatch:")
-print('-' * 50)
-for element in mal_dict:
-    if element not in aniwatch_dict:
-        print(mal_dict[element], element)
+if __name__ == '__main__':
+    with open('.env', 'r') as f:
+        client_id = f.readline().strip()
 
-# For Debugging
-with open('mal_entries.txt', 'w') as fg:
-    try:
-        print(mal_dict, file=fg)
-    except UnicodeEncodeError:
-        pass
+    mal_username = 'mNandhu'  # Your MAL username
+    aniwatch_file_path = 'aniwatch_data.txt'  # Path to the aniwatch export file
+
+    new_user_token = 0  # Set to 1 if you want to get a new user token
+    new_list = 0  # Set to 1 if you want to get a new list from MAL
+
+    mal_dict = get_mal_dict(client_id, mal_username, new_list)
+    aniwatch_dict = get_aniwatch_dict(aniwatch_file_path)
+
+    not_in_mal = find_missing_entries(mal_dict, aniwatch_dict)  # Find the entries in aniwatch that are not in MAL
+    not_in_aniwatch = find_missing_entries(aniwatch_dict, mal_dict)  # Find the entries in MAL that are not in aniwatch
+
+    print(f"Number of entries in MAL: {len(mal_dict)}",
+          f"Number of entries in Aniwatch: {len(aniwatch_dict)}",
+          f"Missing Entries ({len(aniwatch_dict) - len(mal_dict)}):",
+          f"{'-' * 50}",
+          f"In AniWatch but not in MAL:",
+          )
+    for key in not_in_mal:
+        print(f"{key}: {not_in_mal[key]}")
+    print(
+        f"{'-' * 50}",
+        f"{'-' * 50}",
+        f"In MAL but not in Aniwatch:",
+        f"{'-' * 50}", sep='\n')
+    for key in not_in_aniwatch:
+        print(f"{key}: {not_in_aniwatch[key]}")
+
+    if input("Do you want to add the missing entries to your MAL List(y/n)?") == 'y':
+        endpoint = custom_endpoint.EndPoint(client_id)
+        if new_user_token:
+            print(
+                "Adding entries to your MAL List will require you to authorise this application to access your MAL "
+                "account")
+            endpoint.print_new_authorisation_url()  # Print the URL to get the authorisation code
+            endpoint.authorisation_code = input("Enter the authorisation code from the URL: ")
+
+            user_token = endpoint.generate_new_token()
+        else:
+            from json import load as json_load
+
+            with open('token.json', 'r') as file:
+                user_token = json_load(file)
+
+        endpoint.print_user_info(user_token['access_token'])
+        endpoint.add_entry(access_token=user_token['access_token'], anime_id=52701, status='on_hold')
